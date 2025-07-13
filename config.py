6e44e591644f68fa462e2f47d835fd61cf36b9ea
Mohammed-Owais-08 <mdowais0803@gmail.com>
2025-07-13 21:53:14 +0530

    Render DataBase Added

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
 API_KEY='dc86869ebbb9b790c93864f3974ffb2c'
+import os
+from sqlalchemy import create_engine
+
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///weather.db")
+engine = create_engine(DATABASE_URL)
